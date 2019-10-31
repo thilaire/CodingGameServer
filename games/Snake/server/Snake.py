df94969864146063966c9cf6204921213a618f25
Thibault Hilaire <thibault.hilaire@lip6.fr>
2019-10-31 17:26:49 +0100

    make the parameters public

--- games/Snake/server/Snake.py
+++ games/Snake/server/Snake.py
@@ -131,24 +131,23 @@ class Snake(Game):
 
 		# random arena
 		totalSize = randint(40, 60)  # sX + sY is randomly in [30,60]
-		self._L = randint(20, 40)
-		self._H = totalSize - self._L
-		self._L, self._H = max(self._L, self._H), min(self._L, self._H)   # L is greater than H
-		self._arena = Arena(self._L, self._H, int(options.get("difficulty", 1)))
+		self.L = randint(20, 40)
+		self.H = totalSize - self.L
+		self.L, self.H = max(self.L, self.H), min(self.L, self.H)   # L is greater than H
+		self.arena = Arena(self.L, self.H, int(options.get("difficulty", 1)))
 
 		# players positions (list of positions, firsst is the head)
-		self._playerPos = [[(2, self._H//2)], [(self._L-3, self._H//2)]]
+		self.playerPos = [[(2, self.H // 2)], [(self.L - 3, self.H // 2)]]
 
-		self._arena.setPlayer(2, self._H//2, 0)
-		self._arena.setPlayer(self._L-3, self._H//2, 1)
+		self.arena.setPlayer(2, self.H // 2, 0)
+		self.arena.setPlayer(self.L - 3, self.H // 2, 1)
 
 		# counter (one per player), used to know when the snake grows
-		self._counter = [0,0]
+		self.counter = [0,0]
 
 		# call the superclass constructor (only at the end, because the superclass constructor launches
 		# the players and they will immediately requires some Labyrinth's properties)
 		super().__init__(player1, player2, **options)
-
 
 
 	def HTMLrepr(self):
@@ -173,26 +172,26 @@ class Snake(Game):
 		"""
 		# create the lines
 		lines = []
-		for y in range(self._H):
+		for y in range(self.H):
 			lines1 = []
 			lines2 = []
-			for x in range(self._L):
+			for x in range(self.L):
 				# 1st line (with NORTH wall)
-				lines1.append('+-' if self._arena.getWall(x, y, NORTH) else '+ ')
+				lines1.append('+-' if self.arena.getWall(x, y, NORTH) else '+ ')
 				# character of the x,y box
-				pl = self._arena.getPlayer(x, y)
+				pl = self.arena.getPlayer(x, y)
 				strPl = {None: ' ', 0: 'M', 1: 'O'}[pl]
-				if pl is not None and (x, y) == self._playerPos[pl][0]:
+				if pl is not None and (x, y) == self.playerPos[pl][0]:
 					strPl = (Fore.GREEN if pl else Fore.RED) + strPl + Fore.RESET
 				# 2nd line (with WEST wall)
-				lines2.append(('|' if self._arena.getWall(x, y, WEST) else ' ') + strPl)
+				lines2.append(('|' if self.arena.getWall(x, y, WEST) else ' ') + strPl)
 			# add end of the line (EAST walls)
 			lines1.append('+')
-			lines2.append('|' if self._arena.getWall(-1, y, EAST) else ' ')
+			lines2.append('|' if self.arena.getWall(-1, y, EAST) else ' ')
 			lines.append("".join(lines1))
 			lines.append("".join(lines2))
 		# end the last walls in the SOUTH
-		lines.append("".join(('+-' if self._arena.getWall(x, -1, SOUTH) else '+ ') for x in range(self._L))+"+")
+		lines.append("".join(('+-' if self.arena.getWall(x, -1, SOUTH) else '+ ') for x in range(self.L)) + "+")
 
 		#TODO: add colors
 		#TODO: add player names
@@ -222,25 +221,25 @@ class Snake(Game):
 		# move the player
 		pl = self._whoPlays
 		# head position and new position
-		hx, hy = self._playerPos[pl][0]
+		hx, hy = self.playerPos[pl][0]
 		nx = hx + Ddx[direction]
 		ny = hy + Ddy[direction]
 		# check if there is a wall and if the new position is free
-		if self._arena.getWall(hx, hy, direction):
+		if self.arena.getWall(hx, hy, direction):
 			return LOSING_MOVE, "The move make the snakes goes into a wall"
-		if self._arena.getPlayer(nx, ny) is not None:
+		if self.arena.getPlayer(nx, ny) is not None:
 			return LOSING_MOVE, "The move make the snakes collides..."
 		# the snake move
-		self._arena.setPlayer(nx, ny, pl)
-		self._playerPos[pl].insert(0, (nx, ny))
+		self.arena.setPlayer(nx, ny, pl)
+		self.playerPos[pl].insert(0, (nx, ny))
 		# and it may grow or not
-		if self._counter[pl]%10 != 0:
-			qx, qy = self._playerPos[pl].pop()
-			self._arena.setNoone(qx, qy)
+		if self.counter[pl]%10 != 0:
+			qx, qy = self.playerPos[pl].pop()
+			self.arena.setNoone(qx, qy)
 		# increase the counter
-		self._counter[pl] = (self._counter[pl]+1) % 10
+		self.counter[pl] = (self.counter[pl] + 1) % 10
 
-		logger.debug("self._playerPos=%s",self._playerPos)
+		logger.debug("self._playerPos=%s",self.playerPos)
 
 		return NORMAL_MOVE, ""
 
@@ -250,7 +249,7 @@ class Snake(Game):
 		Returns the size of the datas send by getData
 		(for example sizes of arrays, so that the arrays could be allocated before calling getData)
 		"""
-		return "%d %d %d" % (self._L, self._H, len(self._arena.walls))
+		return "%d %d %d" % (self.L, self.H, len(self.arena.walls))
 
 
 
@@ -259,7 +258,7 @@ class Snake(Game):
 		Return the datas of the game (when ask with the GET_GAME_DATA message)
 		ie the list of positions of walls
 		"""
-		return " ".join("%d %d %d %d" % wall for wall in self._arena.walls)
+		return " ".join("%d %d %d %d" % wall for wall in self.arena.walls)
 
 
 
