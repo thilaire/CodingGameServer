40083e84df1981334a0eb64c092708a2d2d03ca7
Thibault Hilaire <thibault.hilaire@lip6.fr>
2019-10-31 16:44:32 +0100

    add an Arena class for the game

--- games/Snake/server/Snake.py
+++ games/Snake/server/Snake.py
@@ -16,64 +16,85 @@ File: Snake.py
 Copyright 2019 T. Hilaire, T. Gautier
 """
 
-from random import randint
+from random import randint, seed
 from server.Constants import NORMAL_MOVE, WINNING_MOVE, LOSING_MOVE
 from .Constants import NORTH, SOUTH, EAST, WEST, Ddx, Ddy
 from server.Game import Game
+from colorama import Fore
+from re import compile
+from itertools import product
 
 # import here your training players
-from .StupidPlayer import StupidPlayer
+from .RandomPlayer import RandomPlayer
+
+import logging
+logger = logging.getLogger("Snake")  # general logger ('root')
+
+regd = compile(r"(\d+)")  # regex to parse a "%d" string
 
 
-
-class Box:
+class Arena:
 	"""
-	the arena is an array of Box (ie integers)
-	arena[x][y] indicates what is at position x,y
+	the arena is an array of integers
+
+	In intern, for each box x,y of the arean
 	- bits 0 to 3 are for the wall (arena[x][y] & (1<<DIR) -> tells if there is a wall in direction DIR)
 	- bits 7 and 8 are for the players 0 and 1
 	the class Box just encapsulates this
 	"""
-	def __init__(self):
-		self._v = 0
-	def setWall(self, direction):
-		self._v |= self._v | (1 << direction)
-	def getWall(self, direction):
-		return bool(self._v & (1 << direction))
-	def setNoone(self):
-		self._v &= 63
-	def setPlayer(self, nPlayer):
-		self._v |= 1 << (7+nPlayer)
-	def getPlayer(self):
-		return {0: None, 1: 0, 2: 1}[self._v >> 7]
+	def __init__(self, L, H, difficulty):
+		"""create a game
+		Returns an arena (array of integers) and a set of walls"""
+		self._L = L
+		self._H = H
+		# create a L*H array of 0)
+		self._array = [[0 for _ in range(H)] for _ in range(L)]
+		self._walls = []
+		# fill with random walls according to the difficulty
+		nbWalls = [0, L*H//10, L*H//4, L*H//2][difficulty]
+		for i in range(nbWalls):
+			x = randint(0, L-1)
+			y = randint(0, H-1)
+			direction = randint(0, 3)
+			self._setWall(x, y, direction)     # no need to check if the wall already exists
+			self._setWall(x + Ddx[direction], y + Ddy[direction], (direction+2)%4)    # wall in the adjacent box
+			self._walls.append((x, y, x + Ddx[direction], y + Ddy[direction]))
+		# remove walls around the start position (just in case)
+		for x, y in [(2, H//2), (L-3, H//2)]:
+			for dx, dy in product(range(-1,2), range(-1,2)):
+				self._removeWall(x+dx, y+dy)
+		# put walls around the arena to bound it
+		for x in range(L):
+			self._setWall(x, 0, NORTH)
+			self._setWall(x, -1, SOUTH)
+		for y in range(H):
+			self._setWall(0, y, WEST)
+			self._setWall(-1, y, EAST)
 
-	def __repr__(self):
-		return bin(self._v)
 
+	@property
+	def walls(self):
+		return self._walls
 
-def createGame(L, H, difficulty):
-	"""create a game
-	Returns an arena (array of integers) and a set of walls"""
-	# create a L*H array of 0)
-	arena = [[Box() for _ in range(H)] for _ in range(L)]
-	walls = []
-	# put walls around the arena to bound it
-	for x in range(L):
-		arena[x][0].setWall(NORTH)
-		arena[x][-1].setWall(SOUTH)
-	for y in range(H):
-		arena[0][y].setWall(WEST)
-		arena[-1][y].setWall(EAST)
-	# fill with walls according to the difficulty
-	nbWalls = [0, L*H//5, L*H//2, L*H][difficulty]
-	for i in range(nbWalls):
-		x = randint(0, L-1)
-		y = randint(0, H-1)
-		direction = randint(0, 3)
-		arena[x][y].setWall(direction)     # no need to check if the wall already exists
-		walls.append((x, y, x + Ddx[direction], y + Ddy[direction]))
+	def _setWall(self, x, y, direction):
+		if -1 <= x < self._L and -1 <= y < self._H:
+			self._array[x][y] |= 1 << direction
 
-	return arena, set(walls)
+	def _removeWall(self, x, y):
+		self._array[x][y] &= 128+64
+
+	def getWall(self, x, y, direction):
+		return bool(self._array[x][y] & (1 << direction))
+
+	def setNoone(self, x, y):
+		self._array[x][y] &= 63
+
+	def setPlayer(self, x, y, nPlayer):
+		self._array[x][y] |= 1 << (6+nPlayer)
+
+	def getPlayer(self, x, y):
+		return {0: None, 1: 0, 2: 1}[self._array[x][y] >> 6]
+
 
 class Snake(Game):
 	"""
@@ -88,16 +109,13 @@ class Snake(Game):
 	- _lastMove, _last_return_code: string and returning code corresponding to the last move
 
 	And some properties
-	- _game: array (list of list of integers) representing the game
-	- _walls: list of walls (only used to send to the players)
+	- _arena: (Arena) array representing the game
 	- _L,_H: length and height of the area
 	- playerPos: list of the coordinates (itself a list) of the two players
 	"""
 
 	# dictionary of the possible training Players (name-> class)
-	type_dict = {"STUPID_PLAYER": StupidPlayer}
-
-
+	type_dict = {"RANDOM_PLAYER": RandomPlayer}
 
 	def __init__(self, player1, player2, **options):
 		"""
@@ -107,14 +125,25 @@ class Snake(Game):
 		:param options: dictionary of options (the options 'seed' and 'timeout' are managed by the Game class)
 		"""
 
-		# random Labyrinth
+		# get a seed if the seed is not given; seed the random numbers generator
+		if 'seed' in options:
+			seed(int(options['seed']))
+
+		# random arena
 		totalSize = randint(40, 60)  # sX + sY is randomly in [30,60]
 		self._L = randint(20, 40)
 		self._H = totalSize - self._L
 		self._L, self._H = max(self._L, self._H), min(self._L, self._H)   # L is greater than H
-		self._arena, self._walls = createGame(self._L, self._H, int(options.get("difficulty", 2)))
+		self._arena = Arena(self._L, self._H, int(options.get("difficulty", 1)))
 
-		#TODO: set the players
+		# players positions (list of positions, firsst is the head)
+		self._playerPos = [[(2, self._H//2)], [(self._L-3, self._H//2)]]
+
+		self._arena.setPlayer(2, self._H//2, 0)
+		self._arena.setPlayer(self._L-3, self._H//2, 1)
+
+		# counter (one per player), used to know when the snake grows
+		self._counter = [0,0]
 
 		# call the superclass constructor (only at the end, because the superclass constructor launches
 		# the players and they will immediately requires some Labyrinth's properties)
@@ -142,19 +171,28 @@ class Snake(Game):
 		"""
 		Convert a Game into string (to be send to clients, and display)
 		"""
+		# create the lines
 		lines = []
 		for y in range(self._H):
 			lines1 = []
 			lines2 = []
 			for x in range(self._L):
-				lines1.append('+-' if self._arena[x][y].getWall(NORTH) else '+ ')
-				pl = {None: ' ', 0: 'X', 1: 'O'}[self._arena[x][y].getPlayer()]
-				lines2.append(('|' if self._arena[x][y].getWall(WEST) else ' ') + pl)
+				# 1st line (with NORTH wall)
+				lines1.append('+-' if self._arena.getWall(x, y, NORTH) else '+ ')
+				# character of the x,y box
+				pl = self._arena.getPlayer(x, y)
+				strPl = {None: ' ', 0: 'M', 1: 'O'}[pl]
+				if pl is not None and (x, y) == self._playerPos[pl][0]:
+					strPl = (Fore.GREEN if pl else Fore.RED) + strPl + Fore.RESET
+				# 2nd line (with WEST wall)
+				lines2.append(('|' if self._arena.getWall(x, y, WEST) else ' ') + strPl)
+			# add end of the line (EAST walls)
 			lines1.append('+')
-			lines2.append('|' if self._arena[x][y].getWall(EAST) else ' ')
+			lines2.append('|' if self._arena.getWall(-1, y, EAST) else ' ')
 			lines.append("".join(lines1))
 			lines.append("".join(lines2))
-		lines.append("".join(('+-' if self._arena[x][-1].getWall(SOUTH) else '+ ') for x in range(self._L))+"+")
+		# end the last walls in the SOUTH
+		lines.append("".join(('+-' if self._arena.getWall(x, -1, SOUTH) else '+ ') for x in range(self._L))+"+")
 
 		#TODO: add colors
 		#TODO: add player names
@@ -170,18 +208,40 @@ class Snake(Game):
 		- move_code: (integer) 0 if the game continues after this move, >0 if it's a winning move, -1 otherwise (illegal move)
 		- msg: a message to send to the player, explaining why the game is ending
 		"""
-		# parse the move and check if it's in correct form
-		# returns the tuple (LOOSING_MOVE, "The move is not in correct form  !") if not valid
+		result = regd.match(move)
+		# check if the data receive is valid
+		if result is None:
+			return LOSING_MOVE, "The move is not in correct form ('%d') !"
 
-		# check if the move is possible
-		# returns (LOOSING_MOVE, "explanations....") if not valid (give the full reason why it is not valid)
+		# get the type and the value
+		direction = int(result.group(1))
+		# check the possible values
+		if not (NORTH <= direction <= WEST):
+			return LOSING_MOVE, "The direction is not valid (should be between 0 and 3!"
 
 		# move the player
-		# update the intern data
-		# use self._whoPlays to get who plays (0 or 1)
+		pl = self._whoPlays
+		# head position and new position
+		hx, hy = self._playerPos[pl][0]
+		nx = hx + Ddx[direction]
+		ny = hy + Ddy[direction]
+		# check if there is a wall and if the new position is free
+		if self._arena.getWall(hx, hy, direction):
+			return LOSING_MOVE, "The move make the snakes goes into a wall"
+		if self._arena.getPlayer(nx, ny) is not None:
+			return LOSING_MOVE, "The move make the snakes collides..."
+		# the snake move
+		self._arena.setPlayer(nx, ny, pl)
+		self._playerPos[pl].insert(0, (nx, ny))
+		# and it may grow or not
+		if self._counter[pl]%10 != 0:
+			qx, qy = self._playerPos[pl].pop()
+			self._arena.setNoone(qx, qy)
+		# increase the counter
+		self._counter[pl] = (self._counter[pl]+1) % 10
 
-		# if won, returns the tuple (WINNING_MOVE, "congratulation message!")
-		# otherwise, just returns (NORMAL_MOVE, "")
+		logger.debug("self._playerPos=%s",self._playerPos)
+
 		return NORMAL_MOVE, ""
 
 
@@ -190,7 +250,7 @@ class Snake(Game):
 		Returns the size of the datas send by getData
 		(for example sizes of arrays, so that the arrays could be allocated before calling getData)
 		"""
-		return "%d %d %d" % (self._L, self._H, len(self._walls))
+		return "%d %d %d" % (self._L, self._H, len(self._arena.walls))
 
 
 
@@ -199,7 +259,7 @@ class Snake(Game):
 		Return the datas of the game (when ask with the GET_GAME_DATA message)
 		ie the list of positions of walls
 		"""
-		return " ".join("%d %d %d %d" % wall for wall in self._walls)
+		return " ".join("%d %d %d %d" % wall for wall in self._arena.walls)
 
 
 
