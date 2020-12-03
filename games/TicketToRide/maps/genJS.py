7b80959351af4494259213dd3569c0901bb55e04
Madour <modar1999@gmail.com>
2020-12-03 09:16:26 +0100

    dynamically resize canvas depending on map size

--- games/TicketToRide/maps/genJS.py
+++ games/TicketToRide/maps/genJS.py
@@ -1,6 +1,7 @@
 # small script to generate the base64 images in JS (from the png)
 
 import base64
+from PIL import Image
 import os
 
 def genJS(filename):
@@ -16,11 +17,17 @@ src = []
 for dir in os.listdir("."):
 	if (os.path.isfile(dir)):
 		continue
-	img64 = str(genJS(dir+'/map.jpg'))
-	res = f"maps['{dir}'] = "
-	res += f"'data:image/jpg;base64,{img64}'"
+	img_path = dir+'/map.jpg'
+	img64 = str(genJS(img_path))
+	img = Image.open(img_path)
+	width, height = img.size
+	img.close()
+	res = "maps['" + dir + "'] = { \n"
+	res += f"  'width' : {width}, 'height' : {height}, \n"
+	res += f"  'data' : 'data:image/jpg;base64,{img64}'\n"
+	res += "}"
 	src.append(res)
 
 with open('../server/templates/game/maps_images.js', 'w') as f:
-	f.write("var maps = [];")
+	f.write("var maps = [];\n")
 	f.write("\n".join(src))
