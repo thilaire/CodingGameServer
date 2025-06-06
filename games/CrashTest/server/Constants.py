c92bf8e11bf7036f11720b8a0e5ac3ff72a88777
benjamin-lamon <benjamin.lamon@etu.sorbonne-universite.fr>
2025-06-06 17:38:58 +0200

    added token colors, will be useful to print interface

--- games/CrashTest/server/Constants.py
+++ games/CrashTest/server/Constants.py
@@ -15,6 +15,8 @@ File: Constants.py
 Copyright 2025 B. Lamon
 """
 
+from colorama import Fore, Back, Style
+
 # 2 pearls, 3 gold, 4 for each gemstone.
 # Pearl, Gold, Blue Sapphire, Diamond (clear),	(Names I've chosen, no clue whether
 # Emerald (green), Ruby (red), Obsidian (black)	there's official color names...)
@@ -47,25 +49,29 @@ RUBY = 6
 OBSIDIAN = 7
 
 
-tokenAmounts = dict()
-"""
-# ugly but works
-for element in tokenTypes:
-    if element == "Pearl":
-        tokenAmounts["Pearl"] = 2
-    elif element == "Gold":
-        tokenAmounts["Gold"] = 3
-    else:
-        tokenAmounts[element] = 4
-"""
-# pretty (dictionary comprehension, but with a weird synthax caus I can read it better)
-tokenAmounts = {
+tokenColors = [
+    Fore.RESET,                                 #
+    Fore.MAGENTA + Style.BRIGHT + Back.BLACK,   # PEARL
+    Fore.YELLOW + Style.BRIGHT + Back.BLACK,    # GOLD
+    Fore.BLUE + Style.NORMAL + Back.BLACK,      # SAPPHIRE
+    Fore.WHITE + Style.NORMAL + Back.BLACK,     # DIAMOND
+    Fore.GREEN + Style.NORMAL + Back.BLACK,     # EMERALD
+    Fore.RED + Style.NORMAL + Back.BLACK,       # RUBY
+    Fore.WHITE + Style.DIM + Back.BLACK         # OBSIDIAN
+]
+
+
+
+# Maximum token amounts. They're the ones we're going to display on the board.
+# (side note: how are we going to display that on a console screen?)
+maxTokenAmounts = {
     token: ( 0 if token == "None"
             else 2 if token == "Pearl"       # 2 pearls
             else 3 if token == "Gold"   # 3 gold tokens
             else 4)                     # 4 gemstone tokens
             for i,token in enumerate(tokenTypes)
 }
+
 
 # Card decks
 # TODO: list every card of the decks
