a968b60d9d5b6783313facea453f3c6ec82de88c
Thibault HILAIRE <thibault.hilaire@lip6.fr>
2026-03-23 16:12:06 +0100

    allow to run CGS in debug mode

--- CGSserver/Webserver.py
+++ CGSserver/Webserver.py
@@ -65,8 +65,10 @@ def runWebServer(host, port):
 	BaseClass.socketio = socketio
 	# TODO: do not run it in a separate thread, but use socketio.start_background_task instead
 	# see https://stackoverflow.com/questions/34581255/python-flask-socketio-send-message-from-thread-not-always-working
-	socketio.run(flask, host=host, port=port, debug=True, use_reloader=False)
-	#socketio.run(flask, host=host, port=port, debug=True, use_reloader=False, allow_unsafe_werkzeug=True)
+	if mode == 'prod':
+		socketio.run(flask, host=host, port=port, debug=True, use_reloader=False)
+	else:
+		socketio.run(flask, host=host, port=port, debug=True, use_reloader=False, allow_unsafe_werkzeug=True)
 
 
 # # ================
